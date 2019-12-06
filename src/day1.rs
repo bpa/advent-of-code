4e3f5e079428a313c8b3e51387ed65126ccc361b
Bruce Armstrong <bruce@clearwateranalytics.com>
2019-12-06 12:24:49 -0700

    Day 2 exactly as written, no clever math

--- src/day1.rs
+++ src/day1.rs
@@ -6,3 +6,24 @@ pub fn fuel_requirements(input: &str) -> u64 {
         .map(|mass| mass / 3 - 2)
         .sum()
 }
+
+#[aoc(day1, part2)]
+pub fn fuel_requirements_including_fuel(input: &str) -> i64 {
+    input
+        .split_whitespace()
+        .map(|mass| mass.parse::<i64>().unwrap())
+        .map(|mass| {
+            let mut total: i64 = 0;
+            let mut fuel = mass;
+            loop {
+                fuel = fuel / 3 - 2;
+                if fuel > 0 {
+                    total += fuel
+                } else {
+                    break;
+                }
+            }
+            total
+        })
+        .sum()
+}
