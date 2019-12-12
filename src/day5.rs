1bfea68d144fd1fde3d248d0a8f845f11d0da492
Bruce Armstrong <bruce@fortressofgeekdom.org>
2019-12-11 21:23:32 -0700

    Got macros working properly

--- src/day5.rs
+++ src/day5.rs
@@ -6,42 +6,94 @@ fn read_memory(input: &str) -> Result<Vec<isize>, ParseIntError> {
 }
 
 #[aoc(day5, part1)]
-pub fn run_12_2(m: &mut [isize]) -> isize {
-    m[1] = 12;
-    m[2] = 2;
+pub fn run_12_2(m: &Vec<isize>) -> isize {
+    let code = m.clone();
 
-    run(m)
+    run(code, vec![1])
 }
 
-macro_rules! instruction {
-    ( $m:ident, $i:ident, | $( $v:pat ),* | $code:block ) => {{
-        $(
-            let $v = $m[$i];
-            $i = $i + 1;
-        )*
-        $code
-    }};
-    ( $m:ident, $i:ident, | $( $v:pat ),* | $code:stmt ) => { instruction!($m, $i, |$($v),*| { $code })};
-    ( $m:ident, $i:ident, | $( $v:pat ),* | $code:expr ) => { instruction!($m, $i, |$($v),*| { $code })};
-}
-
-macro_rules! instruction_set {
-    ($m:ident $($op:pat => $( $code:tt )*,)*) => {
+//This is me abusing macros to learn the ins and outs
+macro_rules! init_registers {
+    ($m:ident) => {
         let mut i: usize = 0;
-        while i < $m.len() {
-            match $m[i] {
-                $( $op => instruction!($m, i, $($code)*) ),*
-            }
+        let mut opcode: usize;
+        let mut mode = Vec::with_capacity(3);
+        macro_rules! i {
+            () => {
+                i
+            };
+        }
+        macro_rules! op {
+            () => {{
+                opcode = $m[i] as usize % 100;
+                let mut op: usize = $m[i] as usize / 100;
+                mode.clear();
+                for _ in 0..3 {
+                    mode.push(op % 10 == 1);
+                    op = op / 10;
+                }
+                opcode
+            }};
+        }
+        macro_rules! get {
+            ( $register:literal ) => {
+                match mode[$register - 1] {
+                    true => i + $register,
+                    false => $m[i + $register] as usize,
+                }
+            };
+        }
+        macro_rules! opcode2 {
+            ( $action2:expr ) => {{
+                let a = get!(1);
+                macro_rules! addr {
+                    () => {
+                        $m[a]
+                    };
+                }
+                $action2;
+                i = i + 2;
+            }};
+        }
+        macro_rules! opcode4 {
+            ( $action4:expr ) => {{
+                let n = get!(1);
+                let v = get!(2);
+                let a = get!(3);
+                macro_rules! noun {
+                    () => {
+                        $m[n]
+                    };
+                }
+                macro_rules! verb {
+                    () => {
+                        $m[v]
+                    };
+                }
+                macro_rules! addr {
+                    () => {
+                        $m[a]
+                    };
+                }
+                $action4;
+                i = i + 4;
+            }};
         }
     };
 }
 
-fn run(m: &mut [isize]) -> isize {
-    instruction_set! { m
-            1 => | noun, verb, addr| { m[addr] = m[noun] + m[verb] },
-            2 => | noun, verb, addr| m[addr] = m[noun] * m[verb],
+fn run(mut m: Vec<isize>, input_data: Vec<isize>) -> isize {
+    let mut input = input_data.iter();
+    init_registers!(m);
+    loop {
+        match op!() {
+            1 => opcode4!(addr!() = noun!() + verb!()),
+            2 => opcode4!(addr!() = noun!() * verb!()),
+            3 => opcode2!(addr!() = *input.next().expect("Ran out of input")),
+            4 => opcode2!(println!("{}: {}", i!(), addr!())),
             99 => break,
-            _ =>  panic!("Invalid instruction found"),
+            _ => panic!("Invalid instruction found"),
+        }
     }
 
     m[0]
