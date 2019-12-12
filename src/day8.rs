d89664b097de0a31f0ee5fa5336bfeb04fd2c1dc
Bruce Armstrong <bruce@clearwateranalytics.com>
2019-12-12 14:08:50 -0700

    Day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -19,3 +19,50 @@ pub fn image_check(input: &str) -> usize {
         .unwrap()
         .1
 }
+
+#[aoc(day8, part2)]
+pub fn show_image(input: &str) -> String {
+    decode_image(input, 25, 6)
+}
+
+fn decode_image(input: &str, width: usize, height: usize) -> String {
+    let mut image = String::with_capacity(width * height + 2);
+    image.push('\n');
+    let bytes = input.as_bytes();
+    let mut start: usize = 0;
+    for _ in 0..height {
+        for _ in 0..width {
+            let mut i = start;
+            while i < input.len() {
+                match bytes[i] as char {
+                    '0' => {
+                        image.push(' ');
+                        break;
+                    }
+                    '1' => {
+                        image.push('X');
+                        break;
+                    }
+                    _ => (),
+                }
+                i = i + width * height;
+            }
+            start = start + 1;
+        }
+        image.push('\n');
+    }
+    image
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn small() {
+        assert_eq!(
+            String::from("\n X\nX \n"),
+            decode_image("0222112222120000", 2, 2)
+        );
+    }
+}
