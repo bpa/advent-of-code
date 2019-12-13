d8e551357a8bd2b36eeb1f2ff284cfb9fe9eb3e2
Bruce Armstrong <bruce@fortressofgeekdom.org>
2019-12-12 21:37:27 -0700

    Having trouble with the borrow checker

--- src/day6.rs
+++ src/day6.rs
@@ -1,39 +1,65 @@
 use std::collections::HashMap;
 
-pub struct Orbit {
-    center: String,
-    satellite: String,
-}
-
 #[aoc_generator(day6)]
-pub fn orbitlist<'a>(input: &'a str) -> Vec<Orbit> {
+pub fn orbitlist<'a>(input: &'a str) -> Vec<(&str, &str)> {
     input
         .split_whitespace()
         .map(|o| {
             let mut planets = o.split(")");
-            Orbit {
-                center: planets.next().unwrap().to_string(),
-                satellite: planets.next().unwrap().to_string(),
-            }
+            (planets.next().unwrap(), planets.next().unwrap())
         })
         .collect()
 }
 
+enum Visited {
+    No,
+    InProgress,
+    Yes,
+}
+
+struct Node<'a> {
+    center: &'a str,
+    satellites: Vec<&'a Node<'a>>,
+    visited: Visited,
+    routes: usize,
+}
+
 #[aoc(day6, part1)]
-pub fn orbits(input: &[Orbit]) -> usize {
-    let mut count: usize = 0;
-    let mut system: HashMap<String, usize> = HashMap::new();
+pub fn orbits(input: &[(&str, &str)]) -> usize {
+    let mut system: HashMap<&str, Node> = HashMap::new();
+
     for orbit in input {
-        let links: usize = match system.get(&orbit.center) {
-            Some(satillites) => *satillites,
-            None => 1,
-        };
-        let satillites: usize = match system.get(&orbit.satellite) {
-            Some(s) => *s,
-            None => 1,
-        };
-        system.insert(orbit.satellite.to_string(), satillites + links);
-        count = count + links;
+        let satellite = system.entry(orbit.1).or_insert_with(|| Node {
+            center: orbit.1,
+            satellites: Vec::new(),
+            visited: Visited::No,
+            routes: 0,
+        });
+
+        system
+            .entry(orbit.0)
+            .and_modify(|e| e.satellites.push(satellite))
+            .or_insert_with(|| Node {
+                center: orbit.0,
+                satellites: vec![satellite],
+                visited: Visited::No,
+                routes: 0,
+            });
     }
-    count
+    system.values_mut().map(|s| s.find_routes()).sum()
+}
+
+impl Node<'_> {
+    fn find_routes(&mut self) -> usize {
+        match self.visited {
+            Visited::Yes => self.routes,
+            Visited::InProgress => panic!("Cycle in graph detected"),
+            Visited::No => {
+                self.visited = Visited::InProgress;
+                self.routes = self.satellites.iter_mut().map(|s| s.find_routes()).sum();
+                self.visited = Visited::Yes;
+                self.routes
+            }
+        }
+    }
 }
