7040315a52210f3d848a7303f4ece15767a3ac12
Bruce Armstrong <bruce@clearwateranalytics.com>
2019-12-19 13:26:00 -0700

    Day 6 part 1

--- src/day6.rs
+++ src/day6.rs
@@ -1,4 +1,5 @@
-use std::cell::{Ref, RefCell, UnsafeCell};
+use std::boxed::Box;
+use std::cell::UnsafeCell;
 use std::collections::HashMap;
 
 #[aoc_generator(day6)]
@@ -18,23 +19,19 @@ pub fn orbitlist(input: &str) -> Vec<(String, String)> {
 #[aoc(day6, part1)]
 fn orbits(input: &[(String, String)]) -> usize {
     let mut system = create_graph(input);
-    println!("Total entries: {}", system.len());
-    system
-        .iter_mut()
-        .map(|s| (*s.borrow_mut()).num_routes())
-        .sum()
+    system.iter_mut().map(|s| s.num_routes()).sum()
 }
 
-fn create_graph(input: &[(String, String)]) -> Vec<RefCell<Node>> {
-    let system: UnsafeCell<Vec<RefCell<Node>>> = UnsafeCell::new(Vec::new());
+fn create_graph(input: &[(String, String)]) -> Vec<Box<Node>> {
+    let system: UnsafeCell<Vec<Box<Node>>> = UnsafeCell::new(Vec::new());
     let mut lookup: HashMap<&str, usize> = HashMap::new();
 
     macro_rules! find {
         ( $name:expr ) => {{
             *lookup.entry($name).or_insert_with(|| unsafe {
-                let s: *mut Vec<RefCell<Node>> = system.get();
+                let s: *mut Vec<Box<Node>> = system.get();
                 let id: usize = (*s).len();
-                (*s).push(RefCell::new(Node::new($name, id)));
+                (*s).push(Box::new(Node::new()));
                 id
             })
         }};
@@ -45,23 +42,12 @@ fn create_graph(input: &[(String, String)]) -> Vec<RefCell<Node>> {
         let planet = find!(&orbit.0);
 
         unsafe {
-            let sys: *mut Vec<RefCell<Node>> = system.get();
-            let sat: &RefCell<Node> = &(*sys)[satellite];
-            let center: Ref<Node> = ((*sys)[planet]).borrow();
-            let satellites: *mut Vec<&RefCell<Node>> = center.satellites.get();
-            print!("{}|{}:", center.pos, center.name);
-            for s in (*satellites).iter() {
-                let b: Ref<Node> = s.borrow();
-                print!(" {}|{}", b.pos, b.name);
-            }
-            println!();
-            (*satellites).push(sat);
-
-            print!("{}|{}:", center.pos, center.name);
-            for s in (*satellites).iter() {
-                print!(" {}|{}", s.borrow().pos, s.borrow().name);
-            }
-            println!();
+            let sys: *mut Vec<Box<Node>> = system.get();
+            let sat: &mut Box<Node> = &mut (*sys)[satellite];
+            let center: &mut Box<Node> = &mut (*sys)[planet];
+            let node: &mut Node = &mut *center;
+            let satellites: *mut Vec<&mut Node> = (*node).satellites.get();
+            (*satellites).push(&mut *sat);
         }
     }
 
@@ -76,18 +62,14 @@ enum Visited {
 }
 
 struct Node<'a> {
-    name: &'a str,
-    pos: usize,
-    satellites: UnsafeCell<Vec<&'a RefCell<Node<'a>>>>,
+    satellites: UnsafeCell<Vec<&'a mut Node<'a>>>,
     visited: Visited,
     routes: usize,
 }
 
 impl<'a> Node<'a> {
-    fn new(name: &'a str, pos: usize) -> Self {
+    fn new() -> Self {
         Node {
-            name: name,
-            pos: pos,
             satellites: UnsafeCell::new(Vec::new()),
             visited: Visited::No,
             routes: 0,
@@ -95,37 +77,16 @@ impl<'a> Node<'a> {
     }
 
     fn num_routes(&mut self) -> usize {
-        println!("1");
-        println!("{} {}: {:?}", self.pos, self.name, self.visited);
         match self.visited {
-            Visited::Yes => {
-                println!("yes");
-                self.routes
-            }
+            Visited::Yes => self.routes,
             Visited::InProgress => {
-                println!("Cycle in graph detected");
-                self.visited = Visited::Yes;
-                0
+                panic!("Cycle in graph detected");
             }
             Visited::No => {
-                println!("no");
-                print!("{}:", self.name);
-                unsafe {
-                    for s in (*self.satellites.get()).iter() {
-                        let sat = s.borrow();
-                        print!(" {}|{}", sat.pos, sat.name);
-                    }
-                }
-                println!();
                 self.visited = Visited::InProgress;
                 let sat = unsafe { &mut *self.satellites.get() };
-                self.routes = sat.len()
-                    + sat
-                        .iter_mut()
-                        .map(|s| (*s.borrow_mut()).num_routes())
-                        .sum::<usize>();
+                self.routes = sat.len() + sat.iter_mut().map(|s| s.num_routes()).sum::<usize>();
                 self.visited = Visited::Yes;
-                println!("Done!");
                 self.routes
             }
         }
@@ -135,18 +96,6 @@ impl<'a> Node<'a> {
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::fmt::{Display, Formatter, Result};
-
-    impl<'a> Display for Node<'a> {
-        fn fmt(&self, f: &mut Formatter) -> Result {
-            write!(f, "{} => {:?}", self.name, unsafe {
-                (*self.satellites.get())
-                    .iter()
-                    .map(|s| s.borrow().name.clone())
-                    .collect::<Vec<&str>>()
-            })
-        }
-    }
 
     macro_rules! s {
         ( $str:ident ) => {
@@ -157,15 +106,9 @@ mod test {
     fn single_link() {
         let input = vec![(s!(a), s!(b)), (s!(b), s!(c)), (s!(b), s!(d))];
         let mut graph = create_graph(&input);
-        for node in graph.iter() {
-            println!("{}", node.borrow());
-        }
-        assert_eq!(
-            4usize,
-            graph
-                .iter_mut()
-                .map(|s| (*s.borrow_mut()).num_routes())
-                .sum()
-        );
+        assert_eq!(2usize, graph[0].num_routes()); //b
+        assert_eq!(3usize, graph[1].num_routes()); //a
+        assert_eq!(0usize, graph[2].num_routes()); //c
+        assert_eq!(0usize, graph[3].num_routes()); //d
     }
 }
