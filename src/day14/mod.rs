fa5354aa038348d9524e21c70d4dd523182d4e85
Bruce Armstrong <bruce@clearwateranalytics.com>
2019-12-19 18:26:38 -0700

    Working parser

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -2,16 +2,16 @@ mod parser;
 use self::parser::formulae;
 
 #[aoc_generator(day14)]
-pub fn requirements(input: &str) -> Vec<(&str, usize), Vec<(&str, usize)>> {
+pub fn requirements(input: &str) -> Vec<((String, usize), Vec<(String, usize)>)> {
     formulae(input).unwrap().1
 }
 
 #[aoc(day14, part1)]
-pub fn fuel_requirements(input: &[((&str, usize), Vec<(&str, usize)>)]) -> u64 {
+pub fn fuel_requirements<'a>(input: &[((String, usize), Vec<(String, usize)>)]) -> usize {
     for i in input {
-        print!("{} {} =>", i.0.0, i.0.1);
-        for r in i.1 {
-            print!(" {}{}", r.0, r.1);
+        print!("{}({}) =>", (i.0).0, (i.0).1);
+        for r in &i.1 {
+            print!(" {}({})", r.0, r.1);
         }
         println!();
     }
--- src/day14/parser.rs
+++ src/day14/parser.rs
@@ -1,27 +1,40 @@
 use nom::character::complete::{alpha1, digit1, multispace0, space1};
-use std::str::FromStr;
+use std::str;
 
 named!(
-    number<usize>,
-    map!(digit1, |s| usize::from_str_radix(s, 10))
+    number<&str, usize>,
+    map!(digit1, |s| usize::from_str_radix(s, 10).unwrap())
 );
 
-// named!(
-//     requirement<(&[u8], usize)>,
-//     do_parse!(num: map!(digit1,  >> space1 >> chem: alpha1 >> ((chem, num)))
-// );
+named!(chemical<&str, String>,
+    map!(alpha1, |s| String::from(s))
+);
 
-// named!(
-//     formula<((&str, usize), Vec<(&str, usize)>)>,
-//     do_parse!(
-//         opt!(tag!(", "))
-//             >> needed: many!(requirement)
-//             >> tag(" => ")
-//             >> wanted: requirement
-//             >> ((wanted, needed))
-//     )
-// );
+named!(
+    requirement<&str, (String, usize)>,
+    do_parse!(opt!(tag!(", ")) >> num: number >> space1 >> chem: chemical >> ((chem, num)))
+);
 
-named!(pub formulae<Vec<((&str, usize), Vec<(&str, usize)>)>>,
+named!(
+    formula<&str, ((String, usize), Vec<(String, usize)>)>,
+    do_parse!(
+        multispace0
+            >> needed: many1!(requirement)
+            >> tag!(" => ")
+            >> wanted: requirement
+            >> (wanted, needed)
+    )
+);
+
+named!(pub formulae<&str, Vec<((String, usize), Vec<(String, usize)>)>>,
     many1!(complete!(formula))
 );
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn parse_number() {
+        assert_eq!(number("4"), Ok(("", 4)));
+    }
+}
